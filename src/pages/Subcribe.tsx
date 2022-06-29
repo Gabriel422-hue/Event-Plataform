98ed5a2d0c7aac5f222e151d112543857ebe4d97
GabrielVSS <gabrielvictor422@hotmail.com>
2022-06-29 09:46:20 -0300

    Finalizando Inscrição e ajuste no css do Sidebar

--- src/pages/Subcribe.tsx
+++ src/pages/Subcribe.tsx
@@ -18,7 +18,7 @@ export function Subcribe(){
     const [name, setName ] = useState('');
     const [email, setEmail ] = useState('');
 
-    const [createSubscriber] = useMutation (CREATE_SUBSCRIBER_MUTATION)
+    const [createSubscriber,{ loading }] = useMutation (CREATE_SUBSCRIBER_MUTATION)
 
     async function handleSubscribe(event: FormEvent){
         event?.preventDefault();
@@ -71,7 +71,8 @@ export function Subcribe(){
 
                     <button 
                     type="submit"
-                    className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                    disabled={loading}
+                    className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
                     >
                     Garantir minha vaga
                     </button>
